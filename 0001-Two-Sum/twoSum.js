066361890feab7845fd16423245c4ff4e81e0d99
Thoraya Elsharawy <thorayaelsharawy@gmail.com>
2023-02-05 20:41:21 +0000

    Find two sum

---
+++ 0001-Two-Sum/twoSum.js
@@ -0,0 +1,9 @@
+function twoSum(nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+     console.log(i , j);
+    }
+  }
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
